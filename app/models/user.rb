6b6a7033e0f00b691c96348bef9cbb03971660db
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-06 02:21:38 +0300

    Add associations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }
 end
