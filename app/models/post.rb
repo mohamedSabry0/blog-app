406147f7032f935cebaefc4f838e75a470b5a242
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-06 02:21:58 +0300

    Add associations for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :text, presence: true, length: { maximum: 1000 }
+  validates :author, presence: true
 end
