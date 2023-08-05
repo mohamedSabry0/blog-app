5ae778f27938ca59eff7bf73cc4fc0fc90578586
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-06 02:22:15 +0300

    Add associations for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :user, presence: true
+  validates :post, presence: true
+  validates :text, presence: true, length: { maximum: 1000 }
 end
