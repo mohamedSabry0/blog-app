626248bc5d790dc3729e75c71281dbee560045fd
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-06 03:09:14 +0300

    Implement comment methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,20 @@ class Comment < ApplicationRecord
   validates :user, presence: true
   validates :post, presence: true
   validates :text, presence: true, length: { maximum: 1000 }
+
+  after_create :update_comments_counter_on_create
+  after_destroy :update_comments_counter_on_destroy
+
+  def update_comments_counter_on_create
+    update_comments_counter(1)
+  end
+
+  def update_comments_counter_on_destroy
+    update_comments_counter(-1)
+  end
+
+  def update_comments_counter(value)
+    post.comments_counter += value
+    post.save
+  end
 end
