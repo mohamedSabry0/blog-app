83f936ea5ab5400bd34a1e3de2de8b2270ade5fd
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-06 03:09:45 +0300

    Implement like methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,4 +4,20 @@ class Like < ApplicationRecord
 
   validates :user, presence: true
   validates :post, presence: true
+
+  after_create :update_likes_counter_on_create
+  after_destroy :update_likes_counter_on_destroy
+
+  def update_likes_counter_on_create
+    update_likes_counter(1)
+  end
+
+  def update_likes_counter_on_destroy
+    update_likes_counter(-1)
+  end
+
+  def update_likes_counter(value)
+    post.likes_counter += value
+    post.save
+  end
 end
