0ef549b92c11ccc5803aefc771d945cd93fac374
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-06 04:36:09 +0300

    Implement suggested changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,15 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
   after_create :update_comments_counter_on_create
   after_destroy :update_comments_counter_on_destroy
+  after_save :update_comments_counter_on_save
+
+  def update_comments_counter_on_save
+    post.comments_counter += 1
+    post.save
+  end
 
   def update_comments_counter_on_create
     update_comments_counter(1)
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,12 @@ class Post < ApplicationRecord
 
   after_create :update_posts_counter_on_create
   after_destroy :update_posts_counter_on_destroy
+  after_save :update_posts_counter_on_save
+
+  def update_posts_counter_on_save
+    author.posts_counter += 1
+    author.save
+  end
 
   def five_most_recent_own_comments
     Comment.where(post: self).order(created_at: :desc).limit(5)
