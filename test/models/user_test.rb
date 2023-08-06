14c59d338d346ae5d6b5d93882bc7c333efc6727
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-08-06 03:41:54 +0300

    Add unit tests for associations and validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save user without name' do
+    user = User.new
+    assert_not user.save
+  end
+
+  test 'should save user with name' do
+    user = User.new
+    user.name = 'John'
+    assert user.save
+  end
+
+  test 'should not save user with negative posts_counter' do
+    user = User.new
+    user.name = 'John'
+    user.posts_counter = -1
+    assert_not user.save
+  end
+
+  test 'should not save user with fractional posts_counter' do
+    user = User.new
+    user.name = 'John'
+    user.posts_counter = 1.5
+    assert_not user.save
+  end
 end
